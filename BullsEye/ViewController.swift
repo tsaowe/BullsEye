467841271d522a4e784d48738f0edfad83efcc95
巍 曹 <tsaowe@weideMacBook-Pro.local>
2015-07-17 11:22:32 +0800

    Initial Commit

---
+++ BullsEye/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by 巍 曹 on 7/17/15.
+//  Copyright (c) 2015 巍 曹. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ BullsEyeTests/BullsEyeTests.swift
@@ -0,0 +1,36 @@
+//
+//  BullsEyeTests.swift
+//  BullsEyeTests
+//
+//  Created by 巍 曹 on 7/17/15.
+//  Copyright (c) 2015 巍 曹. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class BullsEyeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
