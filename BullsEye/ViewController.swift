2a87b49d43645c2d7018f5b1dc5072dfe8ba256a
巍 曹 <tsaowe@weideMacBook-Pro.local>
2015-07-21 18:13:19 +0800

    20150721-181300

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -26,9 +26,32 @@ class ViewController: UIViewController {
     
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         startNewRound()
         updateLabels()
+        
+        
+        // add custome interface to slider
+        let thumbImageNormal = UIImage(named: "SliderThumb-Normal")
+        slider.setThumbImage(thumbImageNormal, forState: .Normal)
+        
+        let thumbImageHighlighted = UIImage(named: "SliderThumb-Highlighted")
+        slider.setThumbImage(thumbImageHighlighted, forState: .Highlighted)
+        
+        let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
+        if let trackLeftImage = UIImage(named: "SliderTrackLeft") {
+            let trackLeftResizable = trackLeftImage.resizableImageWithCapInsets(insets)
+            slider.setMinimumTrackImage(trackLeftResizable, forState: .Normal)
+            
+        }
+        
+        if let trackRightImage = UIImage(named: "SliderTrackRight") {
+            let trackRightResizable = trackRightImage.resizableImageWithCapInsets(insets)
+            slider.setMaximumTrackImage(trackRightResizable, forState: .Normal)
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
