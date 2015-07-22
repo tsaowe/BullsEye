830ecd74c93dd9b8b2ae9ccd4376f9780dca9f44
巍 曹 <tsaowe@weideMacBook-Pro.local>
2015-07-22 15:32:45 +0800

    first toturial

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import QuartzCore
 
 class ViewController: UIViewController {
     
@@ -143,6 +144,13 @@ class ViewController: UIViewController {
     @IBAction func startOver() {
         startNewGame()
         updateLabels()
+        
+        let transition = CATransition()
+        transition.type = kCATransitionFade
+        transition.duration = 1
+        transition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+        view.layer.addAnimation(transition, forKey: nil)
+        
     }
     
 }
