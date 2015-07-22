56b5395025344bd611d31a94540415725f5c7bfb
巍 曹 <tsaowe@weideMacBook-Pro.local>
2015-07-22 11:34:39 +0800

    add webview

--- BullsEye/AboutViewController.swift
+++ BullsEye/AboutViewController.swift
@@ -9,11 +9,20 @@
 import UIKit
 
 class AboutViewController: UIViewController {
+    
+    @IBOutlet weak var webView:UIWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        if let htmlFile = NSBundle.mainBundle().pathForResource("BullsEye",ofType: "html") {
+            let htmlData = NSData(contentsOfFile: htmlFile)
+            let baseURL = NSURL.fileURLWithPath(
+            NSBundle.mainBundle().bundlePath)
+            webView.loadData(htmlData, MIMEType: "text/html", textEncodingName: "UTF-8", baseURL: baseURL)
+        }
     }
 
     override func didReceiveMemoryWarning() {
